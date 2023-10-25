9ba3a666f10c00f60edc3a574cb740e14a271722
Simone Baglivi <sbaglivi@gmail.com>
2023-10-25 23:11:23 +0200

    Some work on a json store

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"os"
 	"time"
@@ -30,37 +31,101 @@ type area struct {
 }
 
 type Store interface {
-	getTasksByArea(int) (error, []task)
-	getSubtaskByTask(int) (error, []subtask)
-	getAllAreas() []area
-	toggleTask(int)
-	toggleSubtask(int)
-	createArea(string) area
-	createTask(int, string) task
-	createSubtask(int, string) subtask
+	GetTasksByArea(int) []task
+	GetSubtaskByTask(int) []subtask
+	GetAllAreas() []area
+	ToggleTask(int)
+	ToggleSubtask(int)
+	CreateArea(string) area
+	CreateTask(int, string) task
+	CreateSubtask(int, string) subtask
+	Init()
 }
 
 type jsonStore struct {
-	fileName string
+	highestId int
+	fileName  string
+	areas     []jsonArea
 }
 
-func (f jsonStore) getAllAreas() []area {
-	jsonFile, err := os.Open(f.fileName)
+/*
+if not capitalized they're not exported ?
+When the first letter is capitalised, the identifier is public to any piece of code that you want to use.
+When the first letter is lowercase, the identifier is private and could only be accessed within the package it was declared.
+https://stackoverflow.com/questions/26327391/json-marshalstruct-returns
+*/
+type jsonArea struct {
+	Id        int
+	Title     string
+	CreatedOn string
+	Tasks     []jsonTask
+}
+
+type jsonTask struct {
+	id          int
+	title       string
+	done        bool
+	createdOn   time.Time
+	completedOn time.Time
+	subtasks    []jsonSubtask
+}
+
+type jsonSubtask struct {
+	id          int
+	title       string
+	done        bool
+	createdOn   time.Time
+	completedOn time.Time
+}
+
+func (s *jsonStore) Init() {
+	jsonFile, err := os.Open(s.fileName)
+	if err != nil {
+		// file does not exist yet
+		s.areas = make([]jsonArea, 0)
+	} else {
+		defer jsonFile.Close()
+		byteValue, _ := io.ReadAll(jsonFile)
+		json.Unmarshal([]byte(byteValue), &s.areas)
+		highestCount := 0
+		for _, a := range s.areas {
+			if a.Id > highestCount {
+				highestCount = a.Id
+			}
+		}
+		s.highestId = highestCount
+	}
+}
+
+func (s *jsonStore) CreateArea(title string) jsonArea {
+	s.areas = append(s.areas, jsonArea{
+		Id:        s.highestId + 1,
+		Title:     title,
+		Tasks:     make([]jsonTask, 0),
+		CreatedOn: time.Now().UTC().Format(time.RFC3339),
+	})
+	s.highestId++
+	return s.areas[len(s.areas)-1]
+}
+
+func (s *jsonStore) Save() {
+	for _, a := range s.areas {
+		fmt.Printf("%+v\n", a)
+	}
+	areasJson, err := json.MarshalIndent(s.areas, "", "  ")
 	if err != nil {
 		panic(err)
 	}
-	defer jsonFile.Close()
-	byteValue, _ := io.ReadAll(jsonFile)
-	var result map[int]interface{}
-	json.Unmarshal([]byte(byteValue), &result)
-	areas := make([]area, len(result))
-	i := 0
-	for k, v := range result {
-		areas[i] = area{id: k, title: v.title, createdOn: v.createdOn}
+	err = os.WriteFile(s.fileName, areasJson, 0644)
+	if err != nil {
+		panic(err)
 	}
-	return areas
 }
 
 func main() {
-
+	store := jsonStore{fileName: "todos.json"}
+	store.Init()
+	store.CreateArea("new area")
+	store.CreateArea("another are")
+	store.Save()
 }
---
+++ notes.md
@@ -0,0 +1,3 @@
+I'm confused because when working with json I don't get to just load part of the todos or of the file.
+I retrieve the whole thing at once. 
+Which makes it seem really inefficient not to just load it all in memory
